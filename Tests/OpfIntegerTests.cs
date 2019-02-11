48657cc5ef65e3ee3414714a32c36efdb5fa0018
Calvin Che <calvin@cczy.io>
2019-02-12 05:55:03 +0800

    Change GEQ and LEQ to GT and LT

--- Tests/OpfIntegerTests.cs
+++ Tests/OpfIntegerTests.cs
@@ -17,13 +17,13 @@ namespace Tests
             var second = oi.GetBucketId(321);
             var third = oi.GetBucketId(890);
 
-            var gt50 = oi.GetBucketsGEQ(50);
+            var gt50 = oi.GetBucketsGT(50);
 
             Assert.Equal(2, gt50.Count);
             Assert.Contains(second, gt50);
             Assert.Contains(third, gt50);
 
-            var lt50 = oi.GetBucketsLEQ(50);
+            var lt50 = oi.GetBucketsLT(50);
             Assert.Single(lt50);
             Assert.Contains(first, lt50);
 
@@ -53,7 +53,7 @@ namespace Tests
         }
 
         [Fact]
-        public void GEQCorrectness()
+        public void GTCorrectness()
         {
             const int iters = 2000;
             var dict = new SortedDictionary<long, long>();
@@ -69,35 +69,20 @@ namespace Tests
             var keys = dict.Keys.ToList();
             for (var i = 0; i < keys.Count; i++)
             {
-                // Inclusive of edge buckets
-                {
-                    var item = keys[i];
-                    var res = oi.GetBucketsGEQ(item);
+                var item = keys[i];
+                var res = oi.GetBucketsGT(item);
 
-                    Assert.Equal(keys.Skip(i)
-                                     .Select(x => dict[x])
-                                     .OrderBy(x => x)
-                                     .Distinct(),
-                                 res.OrderBy(x => x));
-                }
-
-                // Exclusive of edge buckets
-                {
-                    var item = keys[i];
-                    var res = oi.GetBucketsGEQ(item, false);
-
-                    Assert.Equal(keys.Skip(i)
-                                     .Select(x => dict[x])
-                                     .Where(x => x != oi.GetBucketId(item))
-                                     .OrderBy(x => x)
-                                     .Distinct(),
-                                 res.OrderBy(x => x));
-                }
+                Assert.Equal(keys.Skip(i)
+                                 .Where(x => oi.GetBucketRange(item).MaxValue != x)
+                                 .Select(x => dict[x])
+                                 .OrderBy(x => x)
+                                 .Distinct(),
+                             res.OrderBy(x => x));
             }
         }
 
         [Fact]
-        public void LEQCorrectness()
+        public void LTCorrectness()
         {
             const int iters = 2000;
             var dict = new SortedDictionary<long, long>();
@@ -113,30 +98,15 @@ namespace Tests
             var keys = dict.Keys.ToList();
             for (var i = 0; i < keys.Count; i++)
             {
-                // Inclusive of edge buckets
-                {
-                    var item = keys[i];
-                    var res = oi.GetBucketsLEQ(item);
+                var item = keys[i];
+                var res = oi.GetBucketsLT(item);
 
-                    Assert.Equal(keys.Take(i + 1)
-                                     .Select(x => dict[x])
-                                     .OrderBy(x => x)
-                                     .Distinct(),
-                                 res.OrderBy(x => x));
-                }
-
-                // Exclusive of edge buckets
-                {
-                    var item = keys[i];
-                    var res = oi.GetBucketsLEQ(item, false);
-
-                    Assert.Equal(keys.Take(i + 1)
-                                     .Select(x => dict[x])
-                                     .Where(x => x != oi.GetBucketId(item))
-                                     .OrderBy(x => x)
-                                     .Distinct(),
-                                 res.OrderBy(x => x));
-                }
+                Assert.Equal(keys.Take(i + 1)
+                                 .Where(x => oi.GetBucketRange(item).MinValue != x)
+                                 .Select(x => dict[x])
+                                 .OrderBy(x => x)
+                                 .Distinct(),
+                             res.OrderBy(x => x));
             }
         }
 
@@ -157,26 +127,13 @@ namespace Tests
             var keys = dict.Keys.ToList();
             for (var i = 0; i < keys.Count; i++)
             {
-                // Inclusive of edge buckets
                 for (var j = keys.Count - 1; j >= 0; j--)
                 {
                     var item1 = keys[i];
                     var item2 = keys[j];
                     var res = oi.GetBucketsBetween(item1, item2);
-                    var expected = oi.GetBucketsGEQ(Math.Min(item1, item2))
-                        .Intersect(oi.GetBucketsLEQ(Math.Max(item1, item2)));
-
-                    Assert.Equal(expected.OrderBy(x => x), res.OrderBy(x => x));
-                }
-
-                // Exclusive of edge buckets
-                for (var j = keys.Count - 1; j >= 0; j--)
-                {
-                    var item1 = keys[i];
-                    var item2 = keys[j];
-                    var res = oi.GetBucketsBetween(item1, item2, false);
-                    var expected = oi.GetBucketsGEQ(Math.Min(item1, item2), false)
-                        .Intersect(oi.GetBucketsLEQ(Math.Max(item1, item2), false));
+                    var expected = oi.GetBucketsGT(Math.Min(item1, item2))
+                        .Intersect(oi.GetBucketsLT(Math.Max(item1, item2)));
 
                     Assert.Equal(expected.OrderBy(x => x), res.OrderBy(x => x));
                 }
